ba2eff30927aa7da5da0867798f6fd8ee6953cce
deetss <dylan817@live.com>
2019-12-13 16:12:19 -0600

    Major speed increase, added bias tables for pieces

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,178 @@
+const pawnT = [ 
+    [0,  0,  0,  0,  0,  0,  0,  0],
+    [50, 50, 50, 50, 50, 50, 50, 50],
+    [10, 10, 20, 30, 30, 20, 10, 10],
+    [5,  5, 10, 25, 25, 10,  5,  5],
+    [0,  0,  0, 20, 20,  0,  0,  0],
+    [5, -5,-10,  0,  0,-10, -5,  5],
+    [5, 10, 10,-20,-20, 10, 10,  5],
+    [0,  0,  0,  0,  0,  0,  0,  0]
+]
+
+const knightT = [
+    [-50,-40,-30,-30,-30,-30,-40,-50],
+    [-40,-20,  0,  0,  0,  0,-20,-40],
+    [-30,  0, 10, 15, 15, 10,  0,-30],
+    [-30,  5, 15, 20, 20, 15,  5,-30],
+    [-30,  0, 15, 20, 20, 15,  0,-30],
+    [-30,  5, 10, 15, 15, 10,  5,-30],
+    [-40,-20,  0,  5,  5,  0,-20,-40],
+    [-50,-40,-30,-30,-30,-30,-40,-50]
+]
+
+const bishopT = [
+    [-20,-10,-10,-10,-10,-10,-10,-20],
+    [-10,  0,  0,  0,  0,  0,  0,-10],
+    [-10,  0,  5, 10, 10,  5,  0,-10],
+    [-10,  5,  5, 10, 10,  5,  5,-10],
+    [-10,  0, 10, 10, 10, 10,  0,-10],
+    [-10, 10, 10, 10, 10, 10, 10,-10],
+    [-10,  5,  0,  0,  0,  0,  5,-10],
+    [-20,-10,-10,-10,-10,-10,-10,-20]
+]
+
+const rookT = [
+    [0,  0,  0,  0,  0,  0,  0,  0],
+    [5, 10, 10, 10, 10, 10, 10,  5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [-5,  0,  0,  0,  0,  0,  0, -5],
+    [0,  0,  0,  5,  5,  0,  0,  0]
+]
+
+const queenT = [
+    [-20,-10,-10, -5, -5,-10,-10,-20],
+    [-10,  0,  0,  0,  0,  0,  0,-10],
+    [-10,  0,  5,  5,  5,  5,  0,-10],
+    [-5,  0,  5,  5,  5,  5,  0, -5],
+    [0,  0,  5,  5,  5,  5,  0, -5],
+    [-10,  5,  5,  5,  5,  5,  0,-10],
+    [-10,  0,  5,  0,  0,  0,  0,-10],
+    [-20,-10,-10, -5, -5,-10,-10,-20]
+]
+
+const kingMidT = [
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-20,-30,-30,-40,-40,-30,-30,-20],
+    [-10,-20,-20,-20,-20,-20,-20,-10],
+    [20, 20,  0,  0,  0,  0, 20, 20],
+    [20, 30, 10,  0,  0, 10, 30, 20]
+]
+
+const kingEndT = [
+    [-50,-40,-30,-20,-20,-30,-40,-50],
+    [-30,-20,-10,  0,  0,-10,-20,-30],
+    [-30,-10, 20, 30, 30, 20,-10,-30],
+    [-30,-10, 30, 40, 40, 30,-10,-30],
+    [-30,-10, 30, 40, 40, 30,-10,-30],
+    [-30,-10, 20, 30, 30, 20,-10,-30],
+    [-30,-30,  0,  0,  0,  0,-30,-30],
+    [-50,-30,-30,-30,-30,-30,-30,-50]
+]
+
+
 window.onload = function() {
     var board, game = new Chess();
 
     // AI starts here
-    var findBestMove = function(game) {
+    var minimaxRoot = function(depth, game, isMaximizingPlayer) {
         var possibleMoves = game.moves();
-
-        var bestMove = null;
-
-        var bestValue = -9999;
+        var bestMoveFound;
+        var bestMove = -9999;
 
         for(var i = 0; i < possibleMoves.length; i++){
             var possibleMove = possibleMoves[i];
             game.move(possibleMove);
 
-            // take the negative as AI plays black
-            var boardValue = -evaluateBoard(game.board());
+            var value = minimax(depth - 1, game, -10000, 10000, !isMaximizingPlayer);      
             game.undo();
-            if(boardValue > bestValue) {
-                bestValue = boardValue;
-                bestMove = possibleMove;
+            if(value >= bestMove) {
+                bestMove = value;
+                bestMoveFound = possibleMove;
             }
         }
-        return bestMove;
+        return bestMoveFound;
+    };
+
+    var minimax = function(depth, game, alpha, beta, isMaximizingPlayer){
+        positionCount++;
+        if (depth === 0){
+            return -evaluateBoard(game.board());
+        }
+
+        var possibleMoves = game.moves();
+
+        if (isMaximizingPlayer) {
+            var bestMove = -9999;
+            for (var i = 0; i < possibleMoves.length; i++){
+                game.move(possibleMoves[i]);
+                bestMove = Math.max(bestMove, minimax(depth -1, game, alpha, beta, !isMaximizingPlayer));
+                game.undo();
+                alpha = Math.max(alpha, bestMove);
+                if (beta <= alpha){
+                    return bestMove;
+                }
+            }
+            return bestMove;
+        } else {
+            var bestMove = 9999;
+            for (var i = 0; i < possibleMoves.length; i++){
+                game.move(possibleMoves[i]);
+                bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximizingPlayer));
+                game.undo();
+                beta = Math.min(beta, bestMove);
+                if (beta <= alpha) {
+                    return bestMove;
+                }
+            }
+            return bestMove;
+        }
     };
 
     var evaluateBoard = function (board) {
         var totalEvaluation = 0;
         for (var i = 0; i < 8; i++){
             for (var j = 0; j < 8; j++) {
-                totalEvaluation = totalEvaluation + getPieceValue(board[i][j]);
+                totalEvaluation = totalEvaluation + getPieceValue(board[i][j]) + getPositionBias(board[i][j] ,i ,j);
+                
             }
         }
         return totalEvaluation;
     };
+
+    var getPositionBias = function (piece, x, y) {
+        var bias = 0;
+        if (piece === null) {
+            return bias;
+        }
+
+        switch(piece.type) {
+            case 'p':
+                bias = pawnT[x][y];
+                break;
+            case 'r':
+                bias = rookT[x][y];
+                break;
+            case 'n':
+                bias = knightT[x][y];
+                break;
+            case 'b':
+                bias = bishopT[x][y];
+                break;
+            case 'q':
+                bias = queenT[x][y];
+                break;
+            case 'k':
+                bias = kingMidT[x][y];
+                break;
+        }
+        return bias;
+    }
 
     var getPieceValue = function (piece) {
         if (piece === null) {
@@ -41,17 +181,17 @@ window.onload = function() {
 
         var getAbsoluteValue = function (piece) {
             if (piece.type === 'p') {
-                return 10;
+                return 100;
             } else if (piece.type === 'r') {
-                return 50;
+                return 600;
             } else if (piece.type === 'n') {
-                return 30;
+                return 300;
             } else if (piece.type === 'b') {
-                return 30 ;
+                return 400;
             } else if (piece.type === 'q') {
-                return 90;
-            } else if (piece.type === 'k') {
                 return 900;
+            } else if (piece.type === 'k') {
+                return 9000;
             }
             throw "Unknown piece type: " + piece.type;
         };
@@ -77,7 +217,6 @@ window.onload = function() {
             if(game.in_checkmate() === true){
                 alert('Checkmate')
             }
-            alert('Game over');
         }
     };
 
@@ -86,7 +225,18 @@ window.onload = function() {
             alert('Game over');
         }
 
-        var bestMove = findBestMove(game);
+        positionCount = 0;
+        var depth = parseInt($('#search-depth').find(':selected').text());
+
+        var d = new Date().getTime();
+        var bestMove = minimaxRoot(depth, game, true);
+        var d2 = new Date().getTime();
+        var moveTime = (d2 - d);
+        var positionsPerS = ( positionCount * 1000 / moveTime);
+
+        $('#position-count').text(positionCount);
+        $('#time').text(moveTime/1000 + 's');
+        $('#positions-per-s').text(positionsPerS);
         return bestMove;
     };
 
